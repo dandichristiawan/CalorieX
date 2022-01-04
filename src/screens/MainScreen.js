de11f9086491a90fae7c5f02a8b54bc07b5a9f60
Dandi Christiawan <dandi.chrst@gmail.com>
2022-01-04 20:21:49 +0700

    Second Progression

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,32 +1,152 @@
 import React, {Component} from 'react'
-import {Text, View, StyleSheet, PermissionsAndroid} from 'react-native'
-import MapView from 'react-native-maps'
+import {
+  Text,
+  View,
+  StyleSheet,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native'
+import MapView, {
+  Marker,
+  AnimatedRegion,
+  Polyline,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps'
+import haversine from 'haversine'
 
-export default class MainScreen extends Component {
+export default class MainScreen extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
+      coordinate: new AnimatedRegion({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      }),
+    }
+  }
+
+  componentDidMount () {
+    const {coordinate} = this.state
+
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const {routeCoordinates, distanceTravelled} = this.state
+        const {latitude, longitude} = position.coords
+
+        const newCoordinate = {
+          latitude,
+          longitude,
+        }
+
+        if (Platform.OS === 'android') {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(newCoordinate, 500)
+          }
+        } else {
+          coordinate.timing(newCoordinate).start()
+        }
+
+        this.setState({
+          latitude,
+          longitude,
+          routeCoordinates: routeCoordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + this.calcDistance(newCoordinate),
+          prevLatLng: newCoordinate,
+        })
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 10,
+      },
+    )
+  }
+
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  })
+
+  calcDistance = newLatLng => {
+    const {prevLatLng} = this.state
+    return haversine(prevLatLng, newLatLng) || 0
+  }
+
   render () {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+          provider={PROVIDER_GOOGLE}
+          showUserLocation
+          followUserLocation
+          loadingEnabled
+          region={this.getMapRegion()}>
+          <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker
+            }}
+            coordinate={this.state.coordinate}
+          />
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.bubble, styles.button]}>
+            <Text style={styles.bottomBarContent}>
+              {parseFloat(this.state.distanceTravelled).toFixed(2)} km
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map : {
-        flex: 1,
-    },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
 })
-
-
